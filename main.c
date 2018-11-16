f5eadd381440b7e4c14b676ac283b279849b3205
yuminchoi <chldbals219@naver.com>
2018-11-17 01:16:15 +0900

    plus print funtion
    빙고판을 만드는 코드에 빙고판을 화면에 출력하는 코드까지 추가

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 5
+#define N 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void initate_bingo(int bingo[SIZE][SIZE]){
+void initate_bingo(int bingo[N][N]){
 	int i, j, k;
 	
 	srand((unsigned)time(NULL));
-	for (i=0;i<SIZE;i++)
+	for (i=0;i<N;i++)
 	{
-		for (j=0;j<SIZE;j++)
+		for (j=0;j<N;j++)
 		{
-			bingo[i][j]=rand()%(SIZE*SIZE)+1;
-			if(bingo[i][j]==bingo[i][k])
+			bingo[i][j]=rand()%(N*N)+1;
+			for(k=0;k<N;k++)
 			{
-				j--;
-			}	
+				if(bingo[i][j]==bingo[i][k])
+				{
+					j--;
+					break;
+				}
+			}
 		}
 	}
 }
 
 
-void print_bingo(int bingo[SIZE][SIZE])
+void print_bingo(int bingo[N][N]){
+	int i, j;
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			printf("%d ", &bingo[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 
 void get_number_byMe()
 void get_number_byCom()
