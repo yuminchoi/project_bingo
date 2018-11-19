dd22ac4cb395ed0b25675d28a46df4095a77b487
yuminchoi <chldbals219@naver.com>
2018-11-20 00:22:14 +0900

    소소한 오류수정
    여전히 원하는대로 작동x

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #define N 5
 #define WIN 3
+#define SIZE N*N
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -20,16 +21,17 @@ void initate_bingo(int bingo[N][N]){
 	{
 		for (j=0;j<N;j++)
 		{
-			bingo[i][j]=rand()%(N*N)+1;
+
+			bingo[i][j]=rand()%(SIZE)+1;
 			for(k=0;k<N;k++)
 			{
 				for(l=0;l<j;l++)
 				{
 					if(bingo[i][j]==bingo[k][l])
-					{
-						j--;
-						break;
-					}
+						{
+							j--;
+							break;
+						}
 				}
 			}
 		}
@@ -43,7 +45,7 @@ void print_bingo(int bingo[N][N]){
 	{
 		for(j=0;j<N;j++)
 		{
-			printf("%d ", &bingo[i][j]);
+			printf("%d ", bingo[i][j]);
 		}
 		printf("\n");
 	}
@@ -56,17 +58,17 @@ void get_number_byMe(int bingo[N][N], int num){
 	
 	printf("Choose your number: ");
 	scanf("%d", &num);
-	while(1>num||num>(N*N))
+	while(1>num||num>SIZE)
 	{
 		printf("범위밖의 숫자입니다.\n");
 		printf("Choose your number: ");
 		scanf("%d", &num);
 	}
-	for(i=0;i<N;i++)
+/*	for(i=0;i<N;i++)
 	{
 		for(j=0;j<N;j++)
 		{
-			while(bingo[i][j]!=num)
+			if(bingo[i][j]!=num)
 			{
 				printf("이전에 선택했던 숫자입니다.\n");
 				printf("Choose your number: ");
@@ -74,7 +76,7 @@ void get_number_byMe(int bingo[N][N], int num){
 			}				
 		}
 	}
-
+*/
 }
 
 
@@ -144,7 +146,7 @@ void count_bingo(int bingo[N][N], int count){
 	sum=0;
 	for(k=0;k<N;k++)
 	{
-		if(bingo[k][k])
+		if(bingo[k][k]==-1)
 		{
 			sum++;
 		}
@@ -159,7 +161,10 @@ void count_bingo(int bingo[N][N], int count){
 	{
 		for(n=N-1;n>=0;n--)
 		{
-			sum++;
+			if(bingo[m][n]==-1)
+			{
+				sum++;
+			}
 		}
 	}
 	if(sum==5)
@@ -205,7 +210,7 @@ int main(int argc, char *argv[]) {
 			printf("USER WIN!\n");
 			win++;
 		}
-	}while(win<1);
+	}while(win=1);
 	
 	printf("턴 수: %d", turn);
 	
